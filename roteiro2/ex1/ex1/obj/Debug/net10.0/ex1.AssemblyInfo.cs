8a6d8594569781d4b4b0bc786d977387186fb417
Cheeeesyy <gabrielpedroso321@gmail.com>
2026-04-06 22:25:08 -0300

    Roteiros que faltavam

--- roteiro2/ex1/ex1/obj/Debug/net10.0/ex1.AssemblyInfo.cs
+++ roteiro2/ex1/ex1/obj/Debug/net10.0/ex1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ex1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46b989a2b2d565dd48d18020201259c8d9c31bfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("ex1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ex1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
